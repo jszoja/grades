af9d63d2cb51641678c20e1d29d005e56b1fdb26
Jan Szoja <jszoja@gmail.com>
2021-10-03 23:20:34 +0100

    Added unit tests for validation error and /logout

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Student;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Sanctum\Sanctum;
@@ -62,6 +63,36 @@ class ExampleTest extends TestCase
         $response->assertJson(['error' => 'Unsupported json format']);
         $response->assertStatus(400);
 
+    }
+
+    public function test_wrong_format()
+    {
+        $user = User::where('id',1)->first();
+        $token = $user->createToken('mytoken')->plainTextToken;
+
+        Student::$counter=0;
+        $response = $this->postJson('/api/grades', [
+            [ "name" => "John", "grade" => 200 ]
+        ], ['Authorization' => 'Bearer '.$token]);
+
+        $response->assertStatus(422);
+        $response->assertJson(json_decode(
+            '{"errors":{"grade":["The grade must not be greater than 100."],"row":1},"message":"The given data was invalid."}',
+            true
+        ));
+    }
+
+    public function test_logout()
+    {
+        $user = User::where('id',1)->first();
+        $token = $user->createToken('mytoken')->plainTextToken;
+
+        $response = $this->postJson('/api/logout', [
+            [ "name" => "John", "grade" => 200 ]
+        ], ['Authorization' => 'Bearer '.$token]);
+
+        $response->assertStatus(200);
+        $response->assertSee('Successfully logged out');
     }
 
 
